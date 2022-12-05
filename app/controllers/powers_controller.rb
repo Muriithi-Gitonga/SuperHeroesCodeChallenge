beae5a3c664540f34b1ec27aa5e06fe376d1358a
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-12-05 10:39:24 +0300

    done

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,6 +1,6 @@
 class PowersController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response    
 
     def index
         render json: Power.all
